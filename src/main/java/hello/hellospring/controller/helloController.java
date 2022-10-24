4b2e352d48fb0c239e3abac5483ed06c2d050379
shinil <lexshinil1@gmail.com>
2022-10-24 20:27:37 +0900

    inflearn - 섹션 2. MVC와 템플릿 엔진

--- src/main/java/hello/hellospring/controller/helloController.java
+++ src/main/java/hello/hellospring/controller/helloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
@@ -12,4 +13,10 @@ public class helloController {
         model.addAttribute("data", "spring!!"); // 화면과 매핑되는 데이터
         return "hello";// 기본적으로 resouces/templates 밑에 폴더를 찾는다.
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = true) String name, Model model) { // required default는 true, false일 경우 파라미터 필요없다.
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
